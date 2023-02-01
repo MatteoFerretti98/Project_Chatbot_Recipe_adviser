a72850eb35fdcf18cbc70458836a520cd2a067b1
MATTEO FERRETTI <S1083630@studenti.univpm.it>
2023-02-01 16:55:06 +0100

    Implementazione lookipTable + storia1

--- actions/actions.py
+++ actions/actions.py
@@ -9,11 +9,43 @@
 
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
 import pandas as pd
+import random
+
+
 
 pd.set_option('display.max_colwidth', None)
+
+def all_lower(my_list):
+    return [x.lower() for x in my_list]
+
+#df_ingredients = pd.read_csv('./actions/dataset_ricette/ingredienti.csv')
+df_recipe = pd.read_csv('./actions/dataset_ricette/ricette.csv')
+ingredienti_possibili = df_recipe["ing_principale"].tolist()
+
+ingredienti_possibili = list(set(ingredienti_possibili))
+ingredienti_possibili = all_lower(ingredienti_possibili)
+with open("lookup_table_ingredienti.txt", "w") as file:
+        for string in ingredienti_possibili:
+            file.write(string + "\n")
+print(len(ingredienti_possibili))
+
+num_persone_possibili_int = df_recipe["n_persone"].tolist()
+num_persone_possibili = [str(numero) for numero in num_persone_possibili_int]
+num_persone_possibili = list(set(num_persone_possibili))
+#print(num_persone_possibili)
+
+
+portate_possibili = df_recipe["tipo"].tolist()
+portate_possibili = list(set(portate_possibili))
+portate_possibili = all_lower(portate_possibili)
+#print(portate_possibili)
+
+
 
 
 class ActionHelloWorld(Action):
@@ -40,6 +72,71 @@ class MyFallback(Action):
         
         dispatcher.utter_message(response = "utter_fallback")
         return []
+
+
+
+class ValidateRicettaForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_ricetta_form"
+
+    def validate_ingredienti_ricetta(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict):
+        """Validate `ingredienti_ricetta` value."""
+        slot_value = ""
+        i = 0
+        for slot in tracker.latest_message['entities']:
+            i += 1
+            if i == len(tracker.latest_message['entities']):
+                slot_value += slot['value'] 
+                print("dentro if1: " + str(slot_value))
+            else:
+                slot_value += slot['value'] + " "
+                print("dentro if2: " + str(slot_value))
+            print(slot_value)
+        
+        print("new: " + str(tracker.latest_message.get('ingredienti_ricetta')))
+        if slot_value.lower() not in ingredienti_possibili:
+            dispatcher.utter_message(text=f"Mi dispiace ma non ho trovato nessuna ricetta con questo ingrediente.")
+            return {"ingredienti_ricetta": None}
+        dispatcher.utter_message(text=f"Va bene! Cercherò una ricetta con {slot_value}.")
+        return {"ingredienti_ricetta": slot_value}
+
+    def validate_portata_ricetta(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `portata_ricetta` value."""
+        print(slot_value)
+        if slot_value not in portate_possibili:
+            dispatcher.utter_message(text=f"Mi dispiace ma non abbiamo questo tipo di portata tra le nostre ricette. Possiamo accettare: {'/'.join(portate_possibili)}.")
+            return {"portata_ricetta": None}
+        dispatcher.utter_message(text=f"Va bene! Cercherò una ricetta da {slot_value}.")
+        return {"portata_ricetta": slot_value}
+
+    def validate_num_persone_ricetta(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `num_persone_ricetta` value."""
+        print(slot_value)
+        if slot_value not in num_persone_possibili:
+            dispatcher.utter_message(text=f"Mi dispiace, ma non abbiamo ricette per il numero di persone richiesto. Ecco i numeri di persone che accettiamo: {'/'.join(num_persone_possibili)}.")
+            return {"num_persone_ricetta": None}
+        dispatcher.utter_message(text=f"Va bene! Cercherò una ricetta per {slot_value} persone.")
+        return {"num_persone_ricetta": slot_value}
+
+
+
 
 class ActionRicercaPerNome(Action):
     def name(self) -> Text:
